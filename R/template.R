2310f5fb094241a431e2a31f5647701b9bd68da9
jpmoatt <joshua.moatt@defra.gov.uk>
2025-01-30 09:18:29 +0000

    adding roxygen template

---
+++ R/template.R
@@ -0,0 +1,17 @@
+#' @title
+#' Title
+#'
+#' @author
+#'
+#' @description Description of function
+#'
+#' @details Details of function (optional)
+#'
+#' @param argument description add more as needed
+#'
+#' @param argument description add more as needed
+#'
+#' @return
+#' what is the output
+#'
+#' @export
