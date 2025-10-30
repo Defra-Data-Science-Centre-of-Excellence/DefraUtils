efc3860efb0e2270c4e32935eab915ea11902aa3
jpmoatt <joshua.moatt@defra.gov.uk>
2025-10-30 13:29:28 +0000

    added test to uc_volume_get

--- tests/testthat/test-uc-volume-get.R
+++ tests/testthat/test-uc-volume-get.R
@@ -21,3 +21,30 @@ test_that("uc_volume_get stops for failed response", {
     regexp = "Forbidden"
   )
 })
+
+test_that("uc_volume_get runs without error", {
+  dummy_response <- structure(list(status_code = 200), class = "response")
+
+  # Stub httr::GET to return a dummy response
+  mockery::stub(
+    uc_volume_get,
+    "httr::GET",
+    function(...) dummy_response
+  )
+
+  # Stub httr::stop_for_status to do nothing
+  mockery::stub(
+    uc_volume_get,
+    "httr::stop_for_status",
+    function(response) NULL
+  )
+
+  expect_silent(
+    uc_volume_get(
+      workspace = "https://example.com",
+      volume = "my_volume",
+      token = "abc123",
+      out_file = "output.txt"
+    )
+  )
+})
