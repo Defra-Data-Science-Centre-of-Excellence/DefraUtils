961a1e0821e5ae366f4d3b64ea1fd8c4077c8e8a
jpmoatt <joshua.moatt@defra.gov.uk>
2025-11-19 14:31:02 +0000

    adding test to create_script

--- tests/testthat/test-create-script.R
+++ tests/testthat/test-create-script.R
@@ -19,3 +19,23 @@ test_that("create_script writes expected header", {
   expect_true(any(grepl("Email: josh@example.com", content)))
   expect_true(any(grepl("Date Created: 29/10/2025", content)))
 })
+
+test_that("create_script writes expected header when no author details provided", {
+  # Create a temporary file path
+  temp_dir <- tempdir()
+  
+  # Run the function
+  create_script(
+    file_name = "test_script",
+    file_path = temp_dir,
+    date = "29/10/2025"
+  )
+  
+  # Read the file
+  content <- readLines(file.path(temp_dir, "test_script.R"))
+  
+  # Check that author and email are in the file
+  expect_true(any(grepl("Author: Name", content)))
+  expect_true(any(grepl("Email: email", content)))
+  expect_true(any(grepl("Date Created: 29/10/2025", content)))
+})
